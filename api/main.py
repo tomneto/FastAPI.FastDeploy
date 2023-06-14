9bc4facc839ca0f00e356ef8d4cde1e678c80b82
Antonio Andrietti Neto <92389621+tomneto@users.noreply.github.com>
2023-06-14 01:25:03 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -15,21 +15,21 @@ class app(FastAPI):
 		app = self
 
 		# load up the authentication route
-		enable_auth(app)
+		#enable_auth(app)
 
 		# enable cors to work with mongodb and others that require connection over ethernet from Vercel to third party API's
 		#self.cors_state = enable_cors(self)
 
 		# load the router obtained from the router package
-		self.include_router(route)
+		#self.include_router(route)
 
 		# execute initialization methods
-		self.load_doc_settings()
+		#self.load_doc_settings()
 
 	def load_doc_settings(self):
 		if not app_config().show_doc:
-			self.docs_url=None
-			self.redoc_url=None
+			self.docs_url = None
+			self.redoc_url = None
 		elif app_config().show_doc:
 			pass
 
