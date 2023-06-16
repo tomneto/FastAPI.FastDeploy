73791a192ba091fe4b958bccb18a963be2c81e64
Antonio Andrietti Neto <92389621+tomneto@users.noreply.github.com>
2023-06-16 00:52:40 -0300

    disable the unfinished endpoints

--- api/router/public/example/__init__.py
+++ api/router/public/example/__init__.py
@@ -6,7 +6,7 @@ from starlette.requests import Request
 
 from api.controllers.example import fetch_one, create, update, delete
 
-from api.router.public.example.models import Post, Put, Patch, Delete
+from api.router.public.example.models import Post
 from docs.descriptions import loadDescription
 
 route = APIRouter()
@@ -46,46 +46,3 @@ async def examplePost(request: Post):
 		result, status_code = {"message": e}, 500
 
 	return JSONResponse(content=result, status_code=status_code)
-
-
-@route.put("/example", description=loadDescription("example", 'put'), name="Put Example", tags=["Example Tag"])
-async def examplePut(request: Put):
-	try:
-		result = update(request.user, request.password, request.newpassword)
-		if result is not None:
-			result, status_code = {"uuid": result}, 200
-		else:
-			result, status_code = {"message": "Failed on user update"}, 404
-	except Exception as e:
-		result, status_code = {"message": e}, 500
-
-	return JSONResponse(content=result, status_code=status_code)
-
-
-@route.patch("/example", description=loadDescription("example", 'patch'), name="Patch Example", tags=["Example Tag"])
-async def examplePatch(request: Patch):
-	try:
-		result = update(request.user, request.password)
-		if result is not None:
-			result, status_code = {"uuid": result}, 200
-		else:
-			result, status_code = {"message": "Failed on user patch"}, 404
-	except Exception as e:
-		result, status_code = {"message": e}, 500
-
-	return JSONResponse(content=result, status_code=status_code)
-
-
-@route.delete("/example", description=loadDescription("example", 'delete'), name="Delete Example", tags=["Example Tag"])
-async def exampleDelete(request: Delete):
-	try:
-		result = delete(request.user, request.password)
-		if result is not None:
-			result, status_code = {"uuid": result}, 200
-		else:
-			result, status_code = {"message": "Failed on user delete"}, 404
-	except Exception as e:
-		result, status_code = {"message": e}, 500
-
-	return JSONResponse(content=result, status_code=status_code)
-
