44510cb7cc7c75ca5f6a287f40fdff46afc3cee4
Antonio Andrietti Neto <92389621+tomneto@users.noreply.github.com>
2023-06-20 01:17:08 -0300

    vercel json unified

--- api/demo/home.py
+++ api/demo/home.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 from starlette.responses import JSONResponse, HTMLResponse
 
 demo_route = APIRouter()
-templates = Jinja2Templates('api/demo')
+templates = Jinja2Templates('demo')
 
 @demo_route.get("/", include_in_schema=False)
 async def home_doc(req: Request) -> HTMLResponse:
