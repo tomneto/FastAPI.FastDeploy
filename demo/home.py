61a205ae82fb09845bbf685f586e1f5bf1088a1d
Antonio Andrietti Neto <92389621+tomneto@users.noreply.github.com>
2023-06-20 02:35:52 -0300

    test route

--- demo/home.py
+++ demo/home.py
@@ -5,9 +5,9 @@ from fastapi.templating import Jinja2Templates
 from starlette.responses import JSONResponse, HTMLResponse
 
 demo_route = APIRouter()
-templates = Jinja2Templates(os.path.join(os.path.dirname(__file__)))
+#templates = Jinja2Templates(os.path.join(os.path.dirname(__file__)))
 path1 = os.path.join(os.path.dirname(__file__))
-@demo_route.get("/", include_in_schema=False)
-async def home_doc(req: Request) -> HTMLResponse:
-	return templates.TemplateResponse('index.html', {"request": req})
+#@demo_route.get("/", include_in_schema=False)
+#async def home_doc(req: Request) -> HTMLResponse:
+#	return templates.TemplateResponse('index.html', {"request": req})
 
